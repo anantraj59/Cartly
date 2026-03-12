f1f86d0da00e22927b0276ce5242223bcf223939
Anant Raj Patel <anantrajpatel824233@gmail.com>
2026-03-12 20:20:36 +0530

    Updating login system

--- login.js
+++ login.js
@@ -32,16 +32,16 @@ getAuth,
 createUserWithEmailAndPassword,
 signInWithEmailAndPassword,
 sendEmailVerification,
-sendPasswordResetEmail
+sendPasswordResetEmail,
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 const firebaseConfig = {
-
-apiKey: "AIzaSyANXW3CGn3ofb3gzb3CpXCw9oLebaAxiWI",
-authDomain: "cartly-store.firebaseapp.com",
-projectId: "cartly-store",
-appId: "1:836220022823:web:ae9155eacd7d777df21e02"
-
+  apiKey: "AIzaSyANXW3CGn3ofb3gzb3CpXCw9oLebaAxiWI",
+  authDomain: "cartly-store.firebaseapp.com",
+  projectId: "cartly-store",
+  storageBucket: "cartly-store.firebasestorage.app",
+  messagingSenderId: "836220022823",
+  appId: "1:836220022823:web:ae9155eacd7d777df21e02"
 };
 
 const app = initializeApp(firebaseConfig);
@@ -51,8 +51,8 @@ const auth = getAuth(app);
 
 window.signup = function(){
 
-let email = document.getElementById("email").value;
-let password = document.getElementById("password").value;
+let email = document.getElementById("signupEmail").value;
+let password = document.getElementById("signupPassword").value;
 let confirm = document.getElementById("confirmPassword").value;
 
 if(password !== confirm){
@@ -78,8 +78,8 @@ alert("Signup successful. Verify your email.");
 
 window.login = function(){
 
-let email = document.getElementById("email").value;
-let password = document.getElementById("password").value;
+let email = document.getElementById("loginEmail").value;
+let password = document.getElementById("loginPassword").value;
 
 signInWithEmailAndPassword(auth,email,password)
 
@@ -92,13 +92,9 @@ alert("Please verify your email first");
 return;
 }
 
-window.location.href="index.html";
+window.location.replace("index.html");
 
 });
-
-})
-
-window.location.href="index.html";
 
 })
 
@@ -122,12 +118,3 @@ alert("Password reset email sent");
 .catch(err=>alert(err.message));
 
 }
-import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-
-onAuthStateChanged(auth, (user) => {
-
-if(user && user.emailVerified){
-window.location.href="index.html";
-}
-
-});
